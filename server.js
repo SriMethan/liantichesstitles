1257d70da27d5232b886be940fb28d1cc1c1c7bd
easychessanimations <chessbotroulette@gmail.com>
2022-01-09 17:35:16 +0000

    Prettier on commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const MONGODB_URI = process.env.MONGODB_URI
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-console.log(MONGODB_URI)
+console.log(MONGODB_URI);
 
-app.get('/', (req, res) => {
-  res.send('Liatomic titles')
-})
+app.get("/", (req, res) => {
+  res.send("Liatomic titles");
+});
 
 app.listen(PORT, () => {
-  console.log(`Liatomic titles listening at http://localhost:${PORT}`)
-})
+  console.log(`Liatomic titles listening at http://localhost:${PORT}`);
+});
